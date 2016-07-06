7b8647b18475ba6f8dbab08cd46b5c52539d596f
Elaine <elainran@gmail.com>
2016-07-07 01:14:15 +0800

    測試案例來描述出需求

---
+++ Homework/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    public class Product
+    {
+        /// <summary>
+        /// 計算Cost加總
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public List<int> CostAdd(int count)
+        {
+            var cost = new List<int> { 6, 15, 24, 21 };
+
+            return cost;
+        }
+
+        /// <summary>
+        /// 計算Revenue加總
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public List<int> RevenueAdd(int count)
+        {
+            var revenue = new List<int> { 50, 66, 60 };
+
+            return revenue;
+        }
+    }
+}
---
+++ HomeworkTests/ProductTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Homework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Homework.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void Cost_Add_3_Item()
+        {
+            //arrange
+            Product target = new Product();
+            //用三筆計算
+            int count = 3;
+            var expected = new List<int> { 6, 15, 24, 21 };
+
+            //act
+            var actual = target.CostAdd(count);
+
+            //assert
+            CollectionAssert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void Revenue_Add_4_Item()
+        {
+            //arrange
+            Product target = new Product();
+            //用四筆計算
+            int count = 4;
+            var expected = new List<int> { 50, 66, 60 };
+
+            //act
+            var actual = target.RevenueAdd(count);
+
+            //assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
